a21a3635b9b4f7f767dc8231ec45baa8a8b3b46c
Abdullah Adeel <easyautosol@gmail.com>
2023-04-28 03:53:24 +0500

    🤞 initialized project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { MantineProvider } from "@mantine/core";
+
+import { api } from "@/utils/api";
+
+import "@/styles/globals.css";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "dark",
+      }}
+    >
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </MantineProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
