bdb2b9e7e71da3ec073392549c7f60b4b1f88fd5
Abdullah Adeel <easyautosol@gmail.com>
2023-06-05 05:12:43 +0500

    ✨ setup social auth \a frontend  - setup social auth core  - setup frontend

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("social/", views.SocialAuthView.as_view(), name="social_auth"),
+    path("social", views.SocialAuthView.as_view(), name="social_auth"),
 ]
